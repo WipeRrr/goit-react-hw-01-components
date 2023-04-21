5a7d7da66ca016e047476d45eefd78c64341f88e
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-04-21 17:41:45 +0300

    style for 2 task and some for 1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,34 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
 export const Statistics = ({ title, stats }) => {
   return (
-      <section className="statistics">
-          {{ title } && (
-              <h2 className="title">{title}</h2>)}
+    <section className={css.statistics}>
+      {{ title } && <h2 className={css.title}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={css.stat_list}>
         {stats.map(stat => (
-          <li key={stat.id} className="item">
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}</span>
+          <li key={stat.id} className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
+            <span className={css.label}>{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stat: PropTypes.arrayOf({
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
